f5acc5a773ff77ddecaae27546076576ca9c010a
Shuvarthi Hazra <shuvarthih@gmail.com>
2026-04-15 01:00:14 +0530

    fix mime type errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Letter from "./letter";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/letter" element={<Letter />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
